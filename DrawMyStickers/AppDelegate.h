da009015c7796cc258bab9e4d6428e5e13930fec
Pottt <developer@potpot.org>
2016-11-07 18:43:23 +0100

    Initial Commit

---
+++ DrawMyStickers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawMyStickers
+//
+//  Created by betaman on 07/11/2016.
+//  Copyright © 2016 Jerome Pottier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawMyStickers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawMyStickers
+//
+//  Created by betaman on 07/11/2016.
+//  Copyright © 2016 Jerome Pottier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
