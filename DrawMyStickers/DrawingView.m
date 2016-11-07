7f330f718129f8c861b040049ab99aad6b99f666
Pottt <developer@potpot.org>
2016-11-07 19:44:32 +0100

    Add a Drawing View in Main Storyboard

---
+++ DrawMyStickers/DrawingView.m
@@ -0,0 +1,21 @@
+//
+//  DrawingView.m
+//  DrawMyStickers
+//
+//  Created by betaman on 07/11/2016.
+//  Copyright © 2016 Jerome Pottier. All rights reserved.
+//
+
+#import "DrawingView.h"
+
+@implementation DrawingView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
